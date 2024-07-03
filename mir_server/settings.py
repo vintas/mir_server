fc63269294576485c24a802f8226806ada475ad2
Avinash N <vintas@users.noreply.github.com>
2024-07-03 13:23:14 +0530

    Initial package_server app

--- mir_server/settings.py
+++ mir_server/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'package_server'
 ]
 
 MIDDLEWARE = [
@@ -49,7 +50,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'mir_server.urls'
+ROOT_URLCONF = 'package_server.urls'
 
 TEMPLATES = [
     {
